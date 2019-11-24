caf4f6b5c2a74a58e4b918f64288a1a70ea38706
Connie <c.amer@icloud.com>
2019-11-24 15:34:10 +0000

    added files

--- gameoflife/gameoflife.py
+++ gameoflife/gameoflife.py
@@ -13,13 +13,44 @@ class GameOfLife:
 
     def countNeighbours(self, row, column):
         count = 0
-        if ((row - 1 >= 0) and (column - 1 >= 0)):
-            if self.board[row-1][column-1]:
-                count += 1
-        if (row -1 >= 0):
-            if self.board[row-1][column]:
-                count += 1
-        if ((row-1 >= 0) and (column +1 >= 0))
+        try:
+            if ((row - 1 >= 0) and (column - 1 >= 0)):
+                if self.board[row-1][column-1]:
+                    count += 1
+            if (row -1 >= 0):
+                if self.board[row-1][column]:
+                    count += 1
+            if (row -1 >= 0) and (column +1 >= 0):
+                if self.board[row-1][column+1]:
+                    count+= 1
+            if (column -1 >= 0):
+                if self.board[row][column-1]:
+                    count += 1
+            if (column +1 < len(self.board[0])):
+                if self.board[row][column+1]:
+                    count += 1
+
+            # from here on, all "row +1 >= 0" conditions have to change. You need to check if row +1 is strictly smaller than the length of the entire board (the number of rows).
+            # and, all "column +1 >= 0" conditions have to change. You need to check if column +1 < len(self.board[0]).
+            if (row +1 >= 0) and (column -1 >= 0):
+                if self.board[row+1][column-1]:
+                    count += 1
+            if (row +1 >= 0):
+                if self.board[row+1][column]:
+                    count += 1
+            if (row +1 >= 0) and (column +1 >= 0):
+                if self.board[row+1][column+1]:
+                    count += 1
+
+            if count > 3 or count < 2:
+                self.board[row][column] = False
+            else:
+                self.board[row][column] = True
+        except Exception as e:
+            print(e)
+            print("row {}, column {}".format(row, column))
+            print(self.board)
+            raise
 
 
 
